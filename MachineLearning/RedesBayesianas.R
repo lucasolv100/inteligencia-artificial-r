52cdd54b24f86c603120801312078909c57ca70d
Lucas Oliveira Souza <lucas.souza@fab.plus-it>
2019-10-16 13:02:55 -0300

    Adicionado exemplo de redes bayesianas

---
+++ MachineLearning/RedesBayesianas.R
@@ -0,0 +1,13 @@
+#intall.packages("bnlearn")
+library(bnlearn)
+
+#install.packages("caret", dependencies = T)
+library(caret)
+
+res <- hc(insurance)
+plot(res)
+
+modelo <- bn.fit(res, data = insurance)
+modelo$GoodStudent
+
+cpquery(modelo, event = (Accident=="Moderate" | Accident=="Severe"), evidence=(Age=="Senior" & RiskAversion=="Adventurous" & MakeModel == "SportsCar"))
